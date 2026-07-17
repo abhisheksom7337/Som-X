3a44dab6950450117ecbae582790525e90b15d70
ABHISHEK <abhisheksom7337@gmail.com>
2026-07-17 20:18:24 +0530

    remove unused theme toggle button and menu button functionality

--- script.js
+++ script.js
@@ -6,12 +6,3 @@ toggle.onclick=()=>{
 document.body.classList.toggle("dark");
 
 }
-const menu=document.querySelector(".menu-btn");
-
-const nav=document.querySelector("nav ul");
-
-menu.onclick=()=>{
-
-nav.classList.toggle("active");
-
-}
